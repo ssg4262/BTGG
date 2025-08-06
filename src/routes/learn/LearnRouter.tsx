01bb0e6bbaa503aa04472696a0b0e1becdb4b353
고승범 <rhtmdqja4262@naver.com>
2025-08-07 01:03:05 +0900

    깃헙 디플로이추가

--- src/routes/learn/LearnRouter.tsx
+++ src/routes/learn/LearnRouter.tsx
@@ -2,5 +2,5 @@ import {RouteObject} from "react-router-dom";
 import {LearnNFT} from "@/pages/learn/LearnNFT.tsx";
 
 export const LearnRouter : RouteObject[]  = [
-    { path: "/learn", element: <LearnNFT /> },
+    { path: "/BTGG/learn", element: <LearnNFT /> },
 ]
